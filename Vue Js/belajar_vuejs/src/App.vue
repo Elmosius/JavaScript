60eb6c359ba4f006c8cf27fef49dceeea09e5919
Elmosius <2272008@maranatha.ac.id>
2024-05-28 23:41:03 +0700

    belajarvue01

--- Vue Js/belajar_vuejs/src/App.vue
+++ Vue Js/belajar_vuejs/src/App.vue
@@ -1,35 +1,48 @@
 <script setup>
-import { ref } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue' // buat nyimpen data
+import FormToDo from './components/layouts/FormToDo.vue'
+import ToDoList from './components/layouts/ToDoList.vue'
 
-const helloWorld = ref('Hello World!')
-const counter = ref(0)
-const button = ref('btn')
+const todos = ref([]) //  state
 
-function increment() {
-  counter.value++
+onMounted(() => {
+  const data = JSON.parse(localStorage.getItem('todos'))
+  if (data) todos.value = data
+})
+
+function add(data) {
+  console.info(data.todo)
+  todos.value.push({
+    id: crypto.randomUUID(),
+    todo: data.todo
+  })
+  save()
+  data.clear()
+}
+
+function remove(id) {
+  todos.value = todos.value.filter((e) => e.id !== id)
+  save()
+}
+
+function save() {
+  localStorage.setItem('todos', JSON.stringify(todos.value))
 }
 </script>
 
 <template>
-  <h1>
-    {{ helloWorld }}
-  </h1>
-
-  <!-- import -->
-  <!-- <HelloWorld /> -->
-
-  <button :class="button" @click="increment()">
-    {{ counter }}
-  </button>
+  <section class="wrapper">
+    <FormToDo @addTodo="add" />
+    <ToDoList :todos="todos" @remove="remove" />
+  </section>
 </template>
 
 <style scoped>
-.btn {
+.wrapper {
   display: flex;
-  background-color: green;
-  padding: 1rem;
-  text-align: center;
-  color: aliceblue;
-} 
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
 </style>
