f3f1455923f989138c732cf3eb832e4308e51ac2
Elmosius <2272008@maranatha.ac.id>
2025-02-09 15:18:23 +0700

    created: soal 110 - 117

--- Latihan/Codewar/day11.js
+++ Latihan/Codewar/day11.js
@@ -31,4 +31,104 @@ function getCount(str) {
   return count;
 }
 
-console.info(getCount("abracadabra"));
+/**** Soal No 110*****/
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/javascript
+function getAge(inputString) {
+  return parseInt(inputString[0]);
+}
+
+/**** Soal No 111*****/
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/javascript
+function removeEveryOther(arr) {
+  return arr.filter((e, i) => i % 2 === 0);
+}
+
+/**** Soal No 112*****/
+// https://www.codewars.com/kata/583f158ea20cfcbeb400000a/javascript
+function arithmetic(a, b, operator) {
+  let operators = {
+    add: "add",
+    subtract: "subtract",
+    multiply: "multiply",
+    divide: "divide",
+  };
+
+  let result;
+
+  if (operator === operators.add) {
+    result = a + b;
+  } else if (operator === operators.subtract) {
+    result = a - b;
+  } else if (operator === operators.multiply) {
+    result = a * b;
+  } else if (operator === operators.divide) {
+    result = a / b;
+  }
+
+  return result;
+}
+
+/**** Soal No 112*****/
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/javascript
+function count(string) {
+  string = string.split("");
+  let result = {};
+
+  string.forEach((e, i) => {
+    result = {
+      ...result,
+      [e]: result[e] ? result[e] + 1 : 1,
+    };
+  });
+
+  return result;
+}
+
+/**** Soal No 113*****/
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/javascript
+var isSquare = function (n) {
+  return Math.sqrt(n) % 1 === 0;
+};
+
+/**** Soal No 114*****/
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+function hoopCount(n) {
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+}
+
+/**** Soal No 115*****/
+// https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/javascript
+function distinct(a) {
+  return a.filter((e, i) => a.indexOf(e) === i);
+}
+
+/**** Soal No 116*****/
+// https://www.codewars.com/kata/539ee3b6757843632d00026b/javascript
+var capitals = function (word) {
+  let result = [];
+
+  word.split("").forEach((e, i) => {
+    if (e === e.toUpperCase()) {
+      result.push(i);
+    }
+  });
+
+  return result;
+};
+
+/**** Soal No 117*****/
+// https://www.codewars.com/kata/57158fb92ad763bb180004e7/train/javascript
+function rainAmount(mm) {
+  if (mm < 40) {
+    return `You need to give your plant ${40 - mm}mm of water`;
+  } else {
+    return `Your plant has had more than enough water for today!`;
+  }
+}
+
+/**** Soal No 118*****/
+// https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/javascript
+
+
+
+console.info(rainAmount(39));
