99a3a8707b62803b7a2e72a77df86b762759281d
Elmosius <2272008@maranatha.ac.id>
2025-02-12 10:43:52 +0700

    created: belajar Routes - Route

--- React/belajar-router/src/main.jsx
+++ React/belajar-router/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
