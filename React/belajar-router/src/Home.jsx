b912eaf053a79a830416993a3eef492ea45396f3
Elmosius <2272008@maranatha.ac.id>
2025-02-13 18:12:21 +0700

    belajar menggunakan use navigate

--- React/belajar-router/src/Home.jsx
+++ React/belajar-router/src/Home.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router";
+
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate({
+      pathname: "/data",
+    });
+  };
+
   return (
     <div>
       <h2>Home</h2>
       <p>Welcome to Page Home</p>
+      <button onClick={handleClick}>Go To Data Page</button>
     </div>
   );
 }
