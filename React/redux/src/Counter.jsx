41e12298cc2ba4c553de07f116cb3a1f0ca4e00b
Elmosius <2272008@maranatha.ac.id>
2025-02-13 19:50:59 +0700

    belajar create selector manual

--- React/redux/src/Counter.jsx
+++ React/redux/src/Counter.jsx
@@ -1,13 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./counterSlice";
+import { decrement, getCounter, getDoubleCounter, increment } from "./counterSlice";
 
 export default function Counter() {
+  let x = 5;
+
   const counter = useSelector((state) => state.counter);
+  const doubleCounter = useSelector(getDoubleCounter);
+  const xCounter = useSelector((state) => getCounter(state, x));
+
   const dispatch = useDispatch();
 
   return (
     <div>
       <h1>Counter: {counter}</h1>
+      <h1>Double Counter: {doubleCounter}</h1>
+      <h1>
+        {x} Counter: {xCounter}
+      </h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(increment(5))}>Increment + 5</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
--- React/redux/src/counterSlice.js
+++ React/redux/src/counterSlice.js
@@ -3,6 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: 0,
+  selectors: {
+    getDoubleCounter: (state) => state * 2,
+    getCounter: (state, value) => state * value,
+  },
   reducers: {
     increment: (state, action) => {
       if (action.payload) {
@@ -22,3 +26,4 @@ export const counterSlice = createSlice({
 });
 
 export const { increment, decrement } = counterSlice.actions;
+export const { getDoubleCounter, getCounter } = counterSlice.selectors;
