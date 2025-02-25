a5ddc8efc18602e4267f8d477ac0253e94a36944
Elmosius <2272008@maranatha.ac.id>
2025-02-25 19:50:55 +0700

    update

--- Vue Js/belajar-dasar/src/components/HelloVue.vue
+++ Vue Js/belajar-dasar/src/components/HelloVue.vue
@@ -7,13 +7,14 @@ const data = {
   id: "hello",
   class: "hello",
 };
-const buttonDisabled = true;
+const buttonDisabled = false;
 </script>
 
 <template>
   <div>
-    <h1 v-bind="data">{{ hello }}</h1>
-    <h1 :class="data.class" v-html="hello"></h1>
+    <h1 v-bind="data">{{ hello.toUpperCase() }}</h1>
+    <h2>{{ 100 * 200 }}</h2>
+    <h1 :class="data.class" v-html="hello.toLowerCase()"></h1>
     <button :disabled="buttonDisabled">Click Me</button>
   </div>
 </template>
