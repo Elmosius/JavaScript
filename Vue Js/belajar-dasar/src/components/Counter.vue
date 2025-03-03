38b840e1f783521eac942bae0d197ce1fb63d5e1
Elmosius <2272008@maranatha.ac.id>
2025-03-03 20:39:08 +0700

    belajar Reactive

--- Vue Js/belajar-dasar/src/components/Counter.vue
+++ Vue Js/belajar-dasar/src/components/Counter.vue
@@ -1,21 +1,30 @@
 <script setup>
-  import {ref} from "vue";
+import {nextTick, ref} from "vue";
 
   let count = ref({
     value: 0,
     name : 'Elmo'
   });
 
-  function increment() {
+  async function increment() {
     count.value = {
       ...count.value,
       value: count.value.value + 1
     }
+
+    count.value.value++
+    await nextTick();
+    count.value.value++
+    await nextTick();
+    count.value.value++
+    await nextTick();
+
+    console.info('increment count after nextTick', count.value);
   }
 </script>
 
 <template>
-  <h1 >
+  <h1 > {{count.name}}
     {{count.value}}
   </h1>
   <button v-on:click="increment">
---
+++ Vue Js/belajar-dasar/src/components/SayHello.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import {reactive} from "vue";
+
+  const person = reactive({
+    firstName: "",
+    lastName: ""
+  })
+
+
+  function sayHello() {
+    person.firstName = document.getElementById("firstName").value;
+    person.lastName = document.getElementById("lastName").value;
+  }
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="First Name" id="firstName"> <br/>
+    <input type="text" placeholder="First Name" id="lastName"> <br/>
+    <button v-on:click="sayHello">Submit</button>
+
+    <h3>Hello {{person.firstName}} {{person.lastName}}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
