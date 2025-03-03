52992658fd2b99d4e1182753a166bcece3e54907
Elmosius <2272008@maranatha.ac.id>
2025-03-03 21:05:38 +0700

    belaajr computed

--- Vue Js/belajar-dasar/src/components/SayHello.vue
+++ Vue Js/belajar-dasar/src/components/SayHello.vue
@@ -1,28 +1,31 @@
 <script setup>
-  import {reactive} from "vue";
+import { computed, reactive } from "vue";
 
-  const person = reactive({
-    firstName: "",
-    lastName: ""
-  })
+const person = reactive({
+  firstName: "",
+  lastName: "",
+});
 
+// agar tidak terjadi perubahan seluruhnya kita gunakan computed(), kita bisa tau juga nilai sebelumnya
+const fullName = computed((oldValue) => {
+  console.info(oldValue);
+  return `${person.firstName} ${person.lastName}`;
+});
 
-  function sayHello() {
-    person.firstName = document.getElementById("firstName").value;
-    person.lastName = document.getElementById("lastName").value;
-  }
+function sayHello() {
+  person.firstName = document.getElementById("firstName").value;
+  person.lastName = document.getElementById("lastName").value;
+}
 </script>
 
 <template>
   <div>
-    <input type="text" placeholder="First Name" id="firstName"> <br/>
-    <input type="text" placeholder="First Name" id="lastName"> <br/>
+    <input type="text" placeholder="First Name" id="firstName" /> <br />
+    <input type="text" placeholder="Last Name" id="lastName" /> <br />
     <button v-on:click="sayHello">Submit</button>
 
-    <h3>Hello {{person.firstName}} {{person.lastName}}</h3>
+    <h3>Hello {{ fullName }}</h3>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
