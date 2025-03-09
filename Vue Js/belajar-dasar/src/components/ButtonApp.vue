41f5cb4296f3b4d38b7e85d4be1df869b5b7210a
Elmosius <2272008@maranatha.ac.id>
2025-03-09 15:14:41 +0700

    belajar fallthrough attributes

---
+++ Vue Js/belajar-dasar/src/components/ButtonApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MyButton from "./MyButton.vue";
+
+function clickHandler() {
+  alert("Button clicked");
+}
+</script>
+
+<template>
+  <!--  fallthrough elements-->
+  <MyButton @click="clickHandler" name="Elmo"></MyButton>
+</template>
+
+<style scoped></style>
---
+++ Vue Js/belajar-dasar/src/components/MyButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useAttrs } from "vue";
+
+const attr = useAttrs();
+console.info(attr);
+</script>
+
+<template>
+  <div>
+    <button>My Button: {{ attr.name }}</button>
+  </div>
+</template>
+
+<style scoped></style>
