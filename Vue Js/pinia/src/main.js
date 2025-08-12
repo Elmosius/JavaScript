3cbde827eeb4d11752209dad2a0f5937c9b6e839
Elmosius Suli <elmosius18@gmail.com>
2025-08-12 20:18:36 +0700

    Created studi kasus To Do List

--- Vue Js/pinia/src/main.js
+++ Vue Js/pinia/src/main.js
@@ -3,12 +3,37 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { createRouter, createWebHistory } from "vue-router";
+
 import Counter from "./components/Counter.vue";
 import MultipleCounter from "./components/MultipleCounter.vue";
+import ToDoList from "./components/ToDoList.vue";
+import AddTodo from "./components/AddTodo.vue";
+import UpdateTodo from "./components/UpdateTodo.vue";
 
 const pinia = createPinia();
 const router = createRouter({
   routes: [
+    {
+      path: "/todolist",
+      children: [
+        {
+          path: "",
+          component: ToDoList,
+          name: "todolist",
+        },
+        {
+          path: "add",
+          name: "add-todolist",
+          component: AddTodo,
+        },
+        {
+          path: "edit/:id",
+          name: "edit-todolist",
+          component: UpdateTodo,
+          props: true,
+        },
+      ],
+    },
     {
       path: "/counter",
       name: "counter",
